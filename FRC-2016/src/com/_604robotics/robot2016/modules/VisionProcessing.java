f6a1d9fe84d756e9bc8d1312e68ab4c8c6ac97cb
rlee287 <rlee287@yahoo.com>
2016-01-21 12:59:38 -0800

    Added triggers to VisionProcessing as well

--- FRC-2016/src/com/_604robotics/robot2016/modules/VisionProcessing.java
+++ FRC-2016/src/com/_604robotics/robot2016/modules/VisionProcessing.java
@@ -1,6 +1,8 @@
 package com._604robotics.robot2016.modules;
 
 import com._604robotics.robotnik.module.Module;
+import com._604robotics.robotnik.trigger.Trigger;
+import com._604robotics.robotnik.trigger.TriggerMap;
 import com._604robotics.robotnik.action.Action;
 import com._604robotics.robotnik.action.ActionData;
 import com._604robotics.robotnik.action.controllers.ElasticController;
@@ -8,6 +10,7 @@ import com._604robotics.robotnik.action.field.FieldMap;
 
 public class VisionProcessing extends Module
 {
+    private boolean ready;    
     public VisionProcessing()
     {
         this.set(new ElasticController()
@@ -22,6 +25,16 @@ public class VisionProcessing extends Module
                 public void run(ActionData data)
                 {
                     
+                }
+            });
+        }});
+        this.set(new TriggerMap()
+        {{
+            add("Ready2", new Trigger()
+            {
+                public boolean run()
+                {
+                    return ready;
                 }
             });
         }});
