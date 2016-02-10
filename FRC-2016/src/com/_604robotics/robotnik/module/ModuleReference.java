4a1960dbdb5faa34aaa2562c3d7345429c6f4584
Steven Smith <Steveice10@gmail.com>
2016-02-10 15:44:20 -0800

    Remove unused import.

--- FRC-2016/src/com/_604robotics/robotnik/module/ModuleReference.java
+++ FRC-2016/src/com/_604robotics/robotnik/module/ModuleReference.java
@@ -8,8 +8,6 @@ import com._604robotics.robotnik.memory.IndexedTable;
 import com._604robotics.robotnik.trigger.TriggerManager;
 import com._604robotics.robotnik.trigger.TriggerReference;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
 // TODO: Auto-generated Javadoc
 /**
  * The Class ModuleReference.
