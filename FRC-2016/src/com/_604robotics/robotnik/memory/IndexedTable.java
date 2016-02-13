e6243ec5726c13951ad38ac2612ac6620b462a79
Michael Smith <michael@spinda.net>
2016-02-13 04:30:52 -0800

    Update and deprecate IndexedTable

--- FRC-2016/src/com/_604robotics/robotnik/memory/IndexedTable.java
+++ FRC-2016/src/com/_604robotics/robotnik/memory/IndexedTable.java
@@ -5,6 +5,10 @@ import java.util.Set;
 import edu.wpi.first.wpilibj.tables.ITable;
 import edu.wpi.first.wpilibj.tables.TableKeyNotDefinedException;
 
+// TODO: Replace IndexedTable with a regular NetworkTable. Factor out Row.
+//       Manually index Modules, Actions, Data, Triggers (requires
+//       robotnik-inspector updates?).
+@Deprecated
 public class IndexedTable {
     public static IndexedTable getTable (String key) {
         return TableCache.getTable(key);
@@ -26,15 +30,15 @@ public class IndexedTable {
         return TableCache.getSubTable(this.table, key);
     }
     
-    public String  getString  (String key, String  defaultValue) { return this.table.getString (key,  defaultValue); }
-    public double  getNumber  (String key, double  defaultValue) { return this.table.getNumber (key,  defaultValue); }
+    public String  getString  (String key, String  defaultValue) { return this.table.getString (key, defaultValue); }
+    public double  getNumber  (String key, double  defaultValue) { return this.table.getNumber (key, defaultValue); }
     public boolean getBoolean (String key, boolean defaultValue) { return this.table.getBoolean(key, defaultValue); }
-    public Object  getValue   (String key) throws TableKeyNotDefinedException { return this.table.getValue(key); }
+    public byte[]  getRaw     (String key, byte[]  defaultValue) { return this.table.getRaw    (key, defaultValue); }
 
     public void putString  (String key, String  value) { this.table.putString (key, value); this.addKey(key); }
     public void putNumber  (String key, double  value) { this.table.putNumber (key, value); this.addKey(key); }
     public void putBoolean (String key, boolean value) { this.table.putBoolean(key, value); this.addKey(key); }
-    public void putValue   (String key, Object  value) { this.table.putValue  (key, value); this.addKey(key); }
+    public void putRaw     (String key, byte[]  value) { this.table.putRaw    (key, value); this.addKey(key); }
 
     private void addKey (String key) {
         if (!this.keys.contains(key)) {
@@ -69,11 +73,11 @@ public class IndexedTable {
         public String  getString  (String  defaultValue) { return this.source.getString (this.key, defaultValue); }
         public double  getNumber  (double  defaultValue) { return this.source.getNumber (this.key, defaultValue); }
         public boolean getBoolean (boolean defaultValue) { return this.source.getBoolean(this.key, defaultValue); }
-        public Object  getValue   () throws TableKeyNotDefinedException { return this.source.getValue(this.key); }
+        public byte[]  getRaw     (byte[]  defaultValue) { return this.source.getRaw    (this.key, defaultValue); }
 
         public void putString  (String  value) { this.source.putString (this.key, value); }
         public void putNumber  (double  value) { this.source.putNumber (this.key, value); }
         public void putBoolean (boolean value) { this.source.putBoolean(this.key, value); }
-        public void putValue   (Object  value) { this.source.putValue  (this.key, value); }
+        public void putRaw     (byte[]  value) { this.source.putRaw    (this.key, value); }
     }
 }
