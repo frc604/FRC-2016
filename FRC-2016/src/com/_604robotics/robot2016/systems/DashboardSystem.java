328b51d60dc28125df6e84b8b844ea344c58a949
Michael Smith <michael@spinda.net>
2016-02-20 15:21:39 -0800

    Expose shooter charged value on Dashboard

--- FRC-2016/src/com/_604robotics/robot2016/systems/DashboardSystem.java
+++ FRC-2016/src/com/_604robotics/robot2016/systems/DashboardSystem.java
@@ -44,6 +44,7 @@ public class DashboardSystem extends Coordinator {
     	/* Shooter */
     	{
     		this.fill(new DataWire(DashboardOutput.asDouble(), "Current Shooter Speed", modules.getModule("Shooter").getData("Current Speed")));
+    		this.fill(new DataWire(DashboardOutput.asBoolean(), "Shooter Charged", modules.getModule("Shooter").getTrigger("Charged")));
 
     		this.fill(new DataWire(modules.getModule("Shooter").getAction("Shoot"), "Target Speed", modules.getModule("Dashboard").getData("Shooter Target Speed")));
     		this.fill(new DataWire(modules.getModule("Shooter").getAction("Shoot"), "Threshold", modules.getModule("Dashboard").getData("Shooter Threshold")));
