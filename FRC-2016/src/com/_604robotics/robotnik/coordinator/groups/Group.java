d29052d42adeeb2759ae9531a4e894bcefb8b40c
Michael Smith <michael@spinda.net>
2016-02-20 17:59:52 -0800

    Allow nesting of steps

--- FRC-2016/src/com/_604robotics/robotnik/coordinator/groups/Group.java
+++ FRC-2016/src/com/_604robotics/robotnik/coordinator/groups/Group.java
@@ -4,50 +4,34 @@ import com._604robotics.robotnik.coordinator.Coordinator;
 import com._604robotics.robotnik.module.ModuleManager;
 import com._604robotics.robotnik.trigger.TriggerAccess;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class Group.
- */
 public class Group {
-    
-    /** The trigger. */
     private final TriggerAccess trigger;
-    
-    /** The coordinator. */
     private final Coordinator coordinator;
 
-    /**
-     * Instantiates a new group.
-     *
-     * @param trigger the trigger
-     * @param coordinator the coordinator
-     */
     public Group (TriggerAccess trigger, Coordinator coordinator) {
         this.trigger = trigger;
         this.coordinator = coordinator;
     }
     
-    /**
-     * Attach.
-     *
-     * @param modules the modules
-     */
     public void attach (ModuleManager modules) {
         coordinator.attach(modules);
     }
     
-    /**
-     * Update.
-     */
     public void update () {
-        if (trigger.get())
+        if (active()) {
             coordinator.update();
+        }
     }
 
-    /**
-     * Reset.
-     */
     public void reset () {
     	coordinator.stop();
+    }
+    
+    public boolean active () {
+        return trigger.get();
+    }
+    
+    public boolean complete () {
+        return !active() || coordinator.complete();
     }
 }
