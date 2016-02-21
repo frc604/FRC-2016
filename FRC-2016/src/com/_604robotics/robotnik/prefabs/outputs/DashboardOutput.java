3abbbf4f52050f96b14c6dc3439216ea9535cd21
Michael Smith <michael@spinda.net>
2016-02-21 15:29:08 -0800

    Add text display for DashboardOutput

--- FRC-2016/src/com/_604robotics/robotnik/prefabs/outputs/DashboardOutput.java
+++ FRC-2016/src/com/_604robotics/robotnik/prefabs/outputs/DashboardOutput.java
@@ -3,37 +3,55 @@ package com._604robotics.robotnik.prefabs.outputs;
 import com._604robotics.robotnik.data.DataRecipient;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class DashboardOutput implements DataRecipient {
-    private static DashboardOutput doubleInstance = null;
-    private static DashboardOutput booleanInstance = null;
-    private final boolean asBoolean;
-
-    public static DashboardOutput asDouble () {
+public class DashboardOutput {
+    private static DoubleDashboardOutput doubleInstance = null;
+    private static BooleanDashboardOutput booleanInstance = null;
+    
+    private DashboardOutput () {}
+    
+    public static DataRecipient asDouble () {
         if (doubleInstance == null) {
-            doubleInstance = new DashboardOutput(false);
+            doubleInstance = new DoubleDashboardOutput();
         }
         
         return doubleInstance;
     }
-
-    public static DashboardOutput asBoolean () {
+    
+    public static DataRecipient asBoolean () {
         if (booleanInstance == null) {
-            booleanInstance = new DashboardOutput(true);
+            booleanInstance = new BooleanDashboardOutput();
         }
         
         return booleanInstance;
     }
-
-    private DashboardOutput (boolean asBoolean) {
-        this.asBoolean = asBoolean;
+    
+    public static DataRecipient asBoolean (String falseText, String trueText) {
+        return new FancyBooleanDashboardOutput(falseText, trueText);
     }
     
-    @Override
-    public void sendData (String fieldName, double dataValue) {
-        if (this.asBoolean) {
-            SmartDashboard.putBoolean(fieldName, dataValue > 0);
-        } else {
+    private static class DoubleDashboardOutput implements DataRecipient {
+        public void sendData (String fieldName, double dataValue) {
             SmartDashboard.putNumber(fieldName, dataValue);
+        }
+    }
+
+    private static class BooleanDashboardOutput implements DataRecipient {
+        public void sendData (String fieldName, double dataValue) {
+                SmartDashboard.putBoolean(fieldName, dataValue > 0);
+        }
+    }
+
+    private static class FancyBooleanDashboardOutput implements DataRecipient {
+        private final String falseText;
+        private final String trueText;
+        
+        public FancyBooleanDashboardOutput (String falseText, String trueText) {
+            this.falseText = falseText;
+            this.trueText = trueText;
+        }
+
+        public void sendData (String fieldName, double dataValue) {
+            SmartDashboard.putString(fieldName, dataValue > 0 ? trueText : falseText);
         }
     }
 }
