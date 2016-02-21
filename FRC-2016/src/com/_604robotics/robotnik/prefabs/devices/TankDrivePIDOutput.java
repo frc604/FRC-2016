d5a2bed78fc7eeaa36745960009c46867b2ddba3
Michael Smith <michael@spinda.net>
2016-02-20 17:48:09 -0800

    Make TankDrivePIDOutput synchronized

--- FRC-2016/src/com/_604robotics/robotnik/prefabs/devices/TankDrivePIDOutput.java
+++ FRC-2016/src/com/_604robotics/robotnik/prefabs/devices/TankDrivePIDOutput.java
@@ -29,7 +29,7 @@ public class TankDrivePIDOutput {
         this.drive = drive;
     }
     
-    private void update () {
+    private synchronized void update () {
         drive.tankDrive(leftPower, rightPower);
     }
 }
