10723c4a29963192b7431287c7b76192c27ed652
rlee287 <rlee287@yahoo.com>
2016-02-23 12:58:22 -0800

    Added listener, removed unused getRun, and created chargedstack

--- FRC-2016/src/com/_604robotics/robot2016/modules/Vision.java
+++ FRC-2016/src/com/_604robotics/robot2016/modules/Vision.java
@@ -52,11 +52,6 @@ public class Vision extends Module
             }
         }
 
-        public boolean getRun()
-        {
-            return run;
-        }
-
         public void setRun(boolean run)
         {
             this.run = run;
@@ -71,7 +66,9 @@ public class Vision extends Module
     public Vision()
     {
         Timer shootTimer=new Timer();
+        GRIPrun.addTableListenerEx(runAction, NetworkTable.NOTIFY_UPDATE);
         BoolFIFOPopQueue readystack=new BoolFIFOPopQueue(10,0.7);
+        BoolFIFOPopQueue chargedstack=new BoolFIFOPopQueue(50,0.5);
 
         this.set(new TriggerMap()
         {{
