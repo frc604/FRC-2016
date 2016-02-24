04e51a0b11e55f18b43a781da7aa2d89bd1bcc73
Squeakadoodle <squeakadoodle6084@gmail.com>
2016-02-23 19:35:48 -0800

    Changed Pickup into a StateController.

--- FRC-2016/src/com/_604robotics/robot2016/modules/Pickup.java
+++ FRC-2016/src/com/_604robotics/robot2016/modules/Pickup.java
@@ -5,6 +5,7 @@ import com._604robotics.robot2016.constants.Ports;
 import com._604robotics.robotnik.action.Action;
 import com._604robotics.robotnik.action.ActionData;
 import com._604robotics.robotnik.action.controllers.ElasticController;
+import com._604robotics.robotnik.action.controllers.StateController;
 import com._604robotics.robotnik.data.DataMap;
 import com._604robotics.robotnik.module.Module;
 import com._604robotics.robotnik.prefabs.actions.PIDAction;
@@ -40,17 +41,10 @@ public class Pickup extends Module {
             add("Pickup Angle", encoder::getAngle);
         }});
 
-        set(new ElasticController() {{
-            addDefault("Off", new Action() {
-                @Override
-                public void run (ActionData data) {
-                    motors.stopMotor();
-                }
-            });
-
+        set(new StateController() {{
             add("Down", new PIDAction(pid, Calibration.PICKUP_DOWN_ANGLE));
             add("Mid", new PIDAction(pid, Calibration.PICKUP_MID_ANGLE));
-            add("Up", new PIDAction(pid, Calibration.PICKUP_UP_ANGLE));
+            addDefault("Up", new PIDAction(pid, Calibration.PICKUP_UP_ANGLE));
         }});
     }
 }
