bb1c103e3aa3503e1540ee69c76e5af7b0ce11a0
Steven Smith <Steveice10@gmail.com>
2016-02-23 21:27:01 -0800

    Set target speed to 30000.

--- FRC-2016/src/com/_604robotics/robot2016/constants/Calibration.java
+++ FRC-2016/src/com/_604robotics/robot2016/constants/Calibration.java
@@ -10,7 +10,7 @@ public final class Calibration {
     public static final Value SHIFTER_HIGH_GEAR = Value.kForward;
 
     /* Shooter Speed Constants */
-    public static final double SHOOTER_TARGET_SPEED = 50000;
+    public static final double SHOOTER_TARGET_SPEED = 30000;
     public static final double SHOOTER_SPEED_THRESHOLD = 2000;
     public static final double SHOOTER_MINIMUM_CHARGE_TIME = 0.5;
 
