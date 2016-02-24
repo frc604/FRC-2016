2b2fe6e06587f5bb0691cefd106af9aeca66cd16
Squeakadoodle <squeakadoodle6084@gmail.com>
2016-02-23 20:14:28 -0800

    Added debug option in Dashboard Trigger Choices.

--- FRC-2016/src/com/_604robotics/robot2016/modules/Dashboard.java
+++ FRC-2016/src/com/_604robotics/robot2016/modules/Dashboard.java
@@ -30,6 +30,10 @@ public class Dashboard extends Module {
             final DashboardTriggerChoice visionOn = new DashboardTriggerChoice("Vision On");
             add("Vision On", visionOn.addDefault("Vision On"));
             add("Vision Off", visionOn.add("Vision Off"));
+            
+            final DashboardTriggerChoice debugOn = new DashboardTriggerChoice("Debug On");
+            add("Debug Off", debugOn.addDefault("Debug Off"));
+            add("Debug On", debugOn.add("Debug On"));
         }});
 
         this.set(new DataMap() {{
