aa8e28d77e76254ca0e3b8c7c64325e740c03bde
rlee287 <rlee287@yahoo.com>
2016-03-12 12:36:07 -0800

    Change calibration comments
    (cherry picked from commit b0add41b19fb98ecddeeb26126d141659cd00190)

--- FRC-2016/src/com/_604robotics/robot2016/constants/Calibration.java
+++ FRC-2016/src/com/_604robotics/robot2016/constants/Calibration.java
@@ -51,6 +51,6 @@ public final class Calibration {
     public static final double VISION_RIGHTMID = 160+15;
     public static final double VISION_TIMER = 2;
     
-    // PIDActionRamp Ramp Speed
+    /* PIDActionRamp Ramp Speed */
     public static final double INCREMENT = 0.1;
 }
