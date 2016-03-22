2844243917cb4481d562f51108adea28ab196d6a
rlee287 <rlee287@yahoo.com>
2016-03-22 12:45:36 -0700

    Added timer functionality

--- FRC-2016/src/com/_604robotics/robot2016/modules/ResetPickup.java
+++ FRC-2016/src/com/_604robotics/robot2016/modules/ResetPickup.java
@@ -7,17 +7,31 @@ import com._604robotics.robotnik.action.controllers.ElasticController;
 import com._604robotics.robotnik.module.Module;
 import com._604robotics.robotnik.prefabs.devices.MA3A10;
 
+import edu.wpi.first.wpilibj.Timer;
+
 public class ResetPickup extends Module
 {
     private final MA3A10 encoder = new MA3A10(Ports.PICKUP_ENCODER);
+    
+    private Timer countTimer=new Timer();
     
     public ResetPickup()
     {
         set(new ElasticController()
         {{
             add("ResetEncoder", new Action() {
+                public void begin(ActionData data) {
+                    countTimer.reset();
+                    countTimer.start();
+                }
                 public void run(ActionData data) {
-                    encoder.setZeroAngle(encoder.getAngle());
+                    if (countTimer.get()>1.5) {
+                        encoder.setZeroAngle(encoder.getAngle());
+                    }
+                }
+                public void end(ActionData data) {
+                    countTimer.reset();
+                    countTimer.stop();
                 }
             });
         }});
